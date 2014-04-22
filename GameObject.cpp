68a407af03b219a93e362c1d8c6f522c245d0676
Jason Mandelbaum <MandelbaumJ@gmail.com>
2014-04-21 22:38:37 -0400

    Added Gameobject for future testing

---
+++ GameObject.cpp
@@ -0,0 +1,16 @@
+#include "GameObject.h"
+#include <iostream>
+
+GameObject::GameObject(void)
+{
+}
+
+
+GameObject::~GameObject(void)
+{
+}
+
+void GameObject::sayHello()
+{
+	printf("GameObject says hello!");
+}
---
+++ GameObject.h
@@ -0,0 +1,15 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+class GameObject
+{
+public:
+	GameObject(void);
+	~GameObject(void);
+
+	void sayHello();
+private:
+
+};
+
+#endif /*GAMEOBJECT_H*/
